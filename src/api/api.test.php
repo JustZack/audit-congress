6304d205f51e46ad3bdc018a3320ef07083227ff
Zack Jones <zackaryj@rams.colostate.edu>
2024-02-10 10:23:55 -0700

    Move testing functions into namespace,classes,folder

--- src/api/api.test.php
+++ src/api/api.test.php
@@ -1,209 +1,46 @@
 <?php
+//Use the testing namespace
+namespace APITest;
 
 //Load everything needed for any congress API calls all at once
-require_once "../php/audit.congress/autoload.php";
+require_once "test/autoload.php";
 
-//ProPublicaApiTest::getPPMemberVotesTest("M000087");
-//ProPublicaApiTest::getPPMemberTest("M000087");
+//ProPublica::getMemberVotesTest("M000087");
+//ProPublica::getMemberTest("M000087");
 
-//ProPublicaApiTest::getPPBillTest(118, "hr3377");
-//ProPublicaApiTest::getPPVoteTest(115, "senate", 1, 17);
+//ProPublica::getBillTest(118, "hr3377");
+//ProPublica::getVoteTest(115, "senate", 1, 17);
 
-//ProPublicaApiTest::getPPCommitteeTest(115, "senate", "SSAF");
-
-
-
-
-//CongressGovApiTest::getCApiMemberTest("M000087");
-//CongressGovApiTest::getCApiMemberListTest();
-//CongressGovApiTest::getCApiMemberSponsoredLegisTest("M000087");
-//CongressGovApiTest::getCApiMemberCoSponsoredLegisTest("M000087");
-
-//CongressGovApiTest::getCApiBillTest(118, "hr", "3377");
-
-//CongressGovApiTest::getCApiBillListTestNoArg();
-
-//CongressGovApiTest::getCApiBillListTestCongress(118);
-//CongressGovApiTest::getCApiBillListTestFactoryOldest(90);
-//CongressGovApiTest::getCApiBillListTestFactoryNewest(90);
-
-//CongressGovApiTest::getCApiBillListTestCongressType(90, "hr");
-//CongressGovApiTest::getCApiBillListTestCongressType(90, "s");
-//CongressGovApiTest::getCApiBillListTestCongressType(118, "hr");
-
-//CongressGovApiTest::getCApiActionsTest(117, "hr", "3076", true);
-//CongressGovApiTest::getCApiAmendmentsTest(117, "hr", "3076", true);
-//CongressGovApiTest::getCApiTextsTest(117, "hr", "3076", true);
-//CongressGovApiTest::getCApiCosponsorsTest(117, "hr", "3076", true);
-//CongressGovApiTest::getCApiCommitteesTest(117, "hr", "3076");
-//CongressGovApiTest::getCApiRelatedBillsTest(117, "hr", "3076");
-//CongressGovApiTest::getCApiSubjectsTest(117, "hr", "3076");
-//CongressGovApiTest::getCApiSummariesTest(117, "hr", "3076");
-//CongressGovApiTest::getCApiTitlesTest(117, "hr", "3076");
+//ProPublica::getCommitteeTest(115, "senate", "SSAF");
 
 
 
 
-class ProPublicaApiTest {
-    static function getPPMemberVotesTest($bioid) {
-        $mem = new ProPublica\MemberVotes($bioid);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
+//CongressGov::getMemberTest("M000087");
+//CongressGov::getMemberListTest();
+//CongressGov::getMemberSponsoredLegisTest("M000087");
+//CongressGov::getMemberCoSponsoredLegisTest("M000087");
 
+//CongressGov::getBillTest(118, "hr", "3377");
 
-    static function getPPMemberTest($bioid) {
-        $mem = new ProPublica\Member($bioid);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
+//CongressGov::getBillListTestNoArg();
 
+//CongressGov::getBillListTestCongress(118);
+//CongressGov::getBillListTestFactoryOldest(90);
+//CongressGov::getBillListTestFactoryNewest(90);
 
-    static function getPPBillTest($congress, $slug) {
-        $bill = new ProPublica\Bill($congress, $slug);
-        $bill->fetchFromApi();
-        $bill->printAsJson();
-    }
+//CongressGov::getBillListTestCongressType(90, "hr");
+//CongressGov::getBillListTestCongressType(90, "s");
+//CongressGov::getBillListTestCongressType(118, "hr");
 
+//CongressGov::getActionsTest(117, "hr", "3076", true);
+//CongressGov::getAmendmentsTest(117, "hr", "3076", true);
+//CongressGov::getTextsTest(117, "hr", "3076", true);
+//CongressGov::getCosponsorsTest(117, "hr", "3076", true);
+//CongressGov::getCommitteesTest(117, "hr", "3076");
+//CongressGov::getRelatedBillsTest(117, "hr", "3076");
+//CongressGov::getSubjectsTest(117, "hr", "3076");
+//CongressGov::getSummariesTest(117, "hr", "3076");
+//CongressGov::getTitlesTest(117, "hr", "3076");
 
-    static function getPPVoteTest($congress, $chamber, $session, $rollCall) {
-        $vote = new ProPublica\Vote($congress, $chamber, $session, $rollCall);
-        $vote->fetchFromApi();
-        $vote->printAsJson();
-    }
-    
-
-    static function getPPCommitteeTest($congress, $chamber, $committeeId) {
-        $comm = new ProPublica\Committee($congress, $chamber, $committeeId);
-        $comm->fetchFromApi();
-        $comm->printAsJson();
-    }
-}
-
-
-class CongressGovApiTest {
-    static function getCApiMemberTest($bioid) {
-        $mem = new CongressGov\Member($bioid);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiMemberSponsoredLegisTest($bioid) {
-        $mem = new CongressGov\SponsoredLegislation($bioid);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiMemberCoSponsoredLegisTest($bioid) {
-        $mem = new CongressGov\CoSponsoredLegislation($bioid);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiMemberListTest() {
-        $mem = new CongressGov\MemberList();
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiBillTest($congress, $type, $number) {
-        $mem = new CongressGov\Bill($congress, $type, $number);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiBillListTestNoArg() {
-        $mem = new CongressGov\BillList();
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiBillListTestFactoryOldest($congress) {
-        $mem = CongressGov\BillList::getByOldestFirst($congress);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiBillListTestFactoryNewest($congress) {
-        $mem = CongressGov\BillList::getByNewestFirst($congress);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-    static function getCApiBillListTestCongress($congress) {
-        $mem = new CongressGov\BillList($congress);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-
-    static function getCApiBillListTestCongressType($congress, $type) {
-        $mem = new CongressGov\BillList($congress, $type);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-
-    static function getCApiActionsTest($congress, $type, $number, $isBill) {
-        $mem = new CongressGov\Actions($congress, $type, $number, $isBill);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-
-    static function getCApiAmendmentsTest($congress, $type, $number, $isBill) {
-        $mem = new CongressGov\Amendments($congress, $type, $number, $isBill);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-    
-
-    static function getCApiTextsTest($congress, $type, $number, $isBill) {
-        $mem = new CongressGov\Texts($congress, $type, $number, $isBill);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-    
-
-    static function getCApiCosponsorsTest($congress, $type, $number, $isBill) {
-        $mem = new CongressGov\Cosponsors($congress, $type, $number, $isBill);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-
-
-    static function getCApiCommitteesTest($congress, $type, $number) {
-        $mem = new CongressGov\Committees($congress, $type, $number);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-    
-
-    static function getCApiRelatedBillsTest($congress, $type, $number) {
-        $mem = new CongressGov\RelatedBills($congress, $type, $number);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-    
-
-    static function getCApiSubjectsTest($congress, $type, $number) {
-        $mem = new CongressGov\Subjects($congress, $type, $number);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-    
-
-    static function getCApiSummariesTest($congress, $type, $number) {
-        $mem = new CongressGov\Summaries($congress, $type, $number);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-    
-
-    static function getCApiTitlesTest($congress, $type, $number) {
-        $mem = new CongressGov\Titles($congress, $type, $number);
-        $mem->fetchFromApi();
-        $mem->printAsJson();
-    }
-}
 ?>
