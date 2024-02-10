669afd332aaa4088d6adf6aaef9abac4c4df7f3c
Zack Jones <zackaryj@rams.colostate.edu>
2024-02-10 11:32:09 -0700

    make all api includes the same for each api integration

--- src/php/unitedstates.legislators.api/autoload.php
+++ src/php/unitedstates.legislators.api/autoload.php
@@ -2,8 +2,8 @@
 
 define("UNITEDSTATESLEGISLATORS_FOLDER", __DIR__);
 require_once AUDITCONGRESS_FOLDER."\\abstract.api.object.php";
-
 require_once UNITEDSTATESLEGISLATORS_FOLDER."\\api\\unitedstates.legislators.api.php";
+
 require_once UNITEDSTATESLEGISLATORS_FOLDER."\\current\\class.members.php";
 require_once UNITEDSTATESLEGISLATORS_FOLDER."\\current\\class.committees.php";
 require_once UNITEDSTATESLEGISLATORS_FOLDER."\\current\\class.district-offices.php";
