031274c75605a209c289e2b4135ea771795415e0
Zack Jones <zackaryj@rams.colostate.edu>
2024-03-05 21:32:12 -0700

    revise change to setFieldsFromObject

--- src/php/mysql.connector/structures/mysql.row.php
+++ src/php/mysql.connector/structures/mysql.row.php
@@ -9,10 +9,10 @@ namespace MySqlConnector {
         public abstract function getColumns();
         public abstract function getValues();
         function setFieldsFromObject($obj, $keyMustExist = true) {
-            if ($obj != null)
-                foreach ($obj as $key=>$value)
-                    if (($keyMustExist && property_exists($this, $key)) || !$keyMustExist) 
-                        $this->{$key} = $value;
+            if ($obj == null) return;
+            foreach ($obj as $key=>$value)
+                if (($keyMustExist && property_exists($this, $key)) || !$keyMustExist) 
+                    $this->{$key} = $value;
         }
 
         
