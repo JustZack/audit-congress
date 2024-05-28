15c682e889bcf160c2a43f9ee01f6dc0fb5e3a64
Zack Jones <zackaryj@rams.colostate.edu>
2024-05-27 18:23:19 -0600

    add policyArea to bill row

--- src/php/audit.congress/objects/bills/rows/class.bill.row.php
+++ src/php/audit.congress/objects/bills/rows/class.bill.row.php
@@ -13,17 +13,18 @@ namespace AuditCongress {
             $bioguideId,
 
             $title,
+            $policyArea,
 
             $introduced,
             $updated;
     
         public function getColumns() {
-            return ["id","type","congress","number","bioguideId","title","introduced","updated"];
+            return ["id","type","congress","number","bioguideId","title","policyArea","introduced","updated"];
         }
 
         public function getValues() {
-            return [$this->id,$this->type,$this->congress,$this->number,
-            $this->bioguideId,$this->title,$this->introduced,$this->updated];
+            return [$this->id,$this->type,$this->congress,$this->number,$this->bioguideId,
+                    $this->title,$this->policyArea,$this->introduced,$this->updated];
         }
     }
 }
