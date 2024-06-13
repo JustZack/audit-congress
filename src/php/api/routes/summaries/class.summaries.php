c226bc49369bcc8cb685611d570ee00079fc2a1e
Zack Jones <justzackj@gmail.com>
2024-06-12 21:36:32 -0600

    Fix  summaries routes missing types

--- src/php/api/routes/summaries/class.summaries.php
+++ src/php/api/routes/summaries/class.summaries.php
@@ -4,8 +4,8 @@ namespace API {
     class Summaries extends RouteGroup {
         public function __construct() {
             parent::__construct("summaries", "\AuditCongress\BillSummaries");
-            $this->addRoute("getById", ["id"]);
-            $this->addRoute("getByBillId", ["billId"]);
+            $this->addRoute("getById", ["id" => "string"]);
+            $this->addRoute("getByBillId", ["billId" => "string"]);
             $this->addRoute("getByFilter", [], ["congress" => "int", "type" => "string", "number" => "int", "text" => "string"]);
         }
     }
