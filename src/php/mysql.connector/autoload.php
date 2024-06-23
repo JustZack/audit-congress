bc6234810ed4471dc42a4d92371a822d8c6c9ea7
Zack Jones <justzackj@gmail.com>
2024-06-22 21:57:01 -0600

    Fix Query wrapper file naming

--- src/php/mysql.connector/autoload.php
+++ src/php/mysql.connector/autoload.php
@@ -18,7 +18,7 @@ require_once MYSQLCONNECTOR_FOLDER."\\querys\mysql.condition.group.php";
 require_once MYSQLCONNECTOR_FOLDER."\\querys\mysql.insert.group.php";
 require_once MYSQLCONNECTOR_FOLDER."\\querys\mysql.join.clause.php";
 require_once MYSQLCONNECTOR_FOLDER."\\querys\mysql.where.clause.php";
-require_once MYSQLCONNECTOR_FOLDER."\\querys\query.wrapper.php";
+require_once MYSQLCONNECTOR_FOLDER."\\querys\mysql.query.wrapper.php";
 
 require_once MYSQLCONNECTOR_FOLDER."\\structures\mysql.row.php";
 require_once MYSQLCONNECTOR_FOLDER."\\structures\mysql.schema.php";
